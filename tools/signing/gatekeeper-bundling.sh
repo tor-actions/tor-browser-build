2941c888ad137e492c7b35d51b903b37ebd1f30a
Pier Angelo Vendrame <pierov@torproject.org>
2023-09-05 16:52:09 +0200

    Bug 40943: Update libdmg-hfs to drop our patch
    We have uplifted one of our libdmg-hfs patches, so we can update our
    git_hash to the upstream commit that includes it.
    Also, deleted the OpenSSL 1.0.2 project in favor of a patch to
    optionally completely disable OpenSSL, since we do not need the
    functionalities it provides.

--- tools/signing/gatekeeper-bundling.sh
+++ tools/signing/gatekeeper-bundling.sh
@@ -39,7 +39,7 @@ test -f $faketime_path || \
   exit_error "$faketime_path is missing"
 test -d $macos_stapled_dir || \
   exit_error "The stapled macos zip files should be placed in directory $macos_stapled_dir"
-libdmg_file="$script_dir/../../out/libdmg-hfsplus/libdmg-hfsplus-2ee327795680-555a7e.tar.gz"
+libdmg_file="$script_dir/../../out/libdmg-hfsplus/libdmg-hfsplus-2cb30de5b277-68b0fc.tar.zst"
 test -f "$libdmg_file" || \
   exit_error "$libdmg_file is missing." \
              "You can build it with:" \
