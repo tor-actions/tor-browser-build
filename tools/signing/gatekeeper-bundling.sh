263c49367721b56099118d78c7e264e504368482
Nicolas Vigier <boklm@torproject.org>
2024-04-10 19:29:10 +0200

    Bug 41124: Fix copying .DS_Store in gatekeeper-bundling.sh
    Fixing some issue introduced in 4e2b66e977873c46b3678c93ae9173f53503f81a.

--- tools/signing/gatekeeper-bundling.sh
+++ tools/signing/gatekeeper-bundling.sh
@@ -69,6 +69,9 @@ export PATH="$PATH:$tmpdir/libdmg-hfsplus:$tmpdir/hfsplus-tools"
 
 cd $tmpdir/dmg
 
+cp ${tbb_version_type}.DS_Store .DS_Store
+rm *.DS_Store
+
 tar -xf $macos_stapled_dir/"${proj_name}-${tbb_version}-notarized+stapled.tar.zst"
 
 cd ..

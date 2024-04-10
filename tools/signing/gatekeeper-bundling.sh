tor-actions/tor-browser-build

commit 4e2b66e977873c46b3678c93ae9173f53503f81a

Bug 40569: Include the channel name on macOS dmgs.
This commits make alpha and nightlies macOS build include the channel
in the app bundle.

Also, it renames the Project_Name_Channel to display_name to make it
less ambiguous, and now Project_Name_Channel includes the channel also
for the release target, even though it will be rarely used.

tools/signing/gatekeeper-bundling.sh
@@ -55,6 +55,7 @@ test -f "$hfstools_file" || \
 ProjName=$(ProjectName)
 Proj_Name=$(Project_Name)
 proj_name=$(project-name)
+disp_name=$(display_name)
 
 test -d "$macos_signed_dir" || mkdir "$macos_signed_dir"
 tmpdir="$macos_stapled_dir/tmp"
@@ -71,8 +72,8 @@ cd $tmpdir/dmg
 tar -xf $macos_stapled_dir/"${proj_name}-${tbb_version}-notarized+stapled.tar.zst"
 
 cd ..
-$script_dir/ddmg.sh $macos_signed_dir/${proj_name}-macos-${tbb_version}.dmg $tmpdir/dmg/ "$Proj_Name"
-rm -rf "dmg/$Proj_Name.app"
+$script_dir/ddmg.sh $macos_signed_dir/${proj_name}-macos-${tbb_version}.dmg $tmpdir/dmg/ "$disp_name"
+rm -rf "dmg/$disp_name.app"
 rm -Rf "$tmpdir"
 
 # move the signed+stapled dmgs to expected output directory for publishing and mar generation

commit 263c49367721b56099118d78c7e264e504368482

Bug 41124: Fix copying .DS_Store in gatekeeper-bundling.sh
Fixing some issue introduced in 4e2b66e977873c46b3678c93ae9173f53503f81a.

tools/signing/gatekeeper-bundling.sh
@@ -69,6 +69,9 @@ export PATH="$PATH:$tmpdir/libdmg-hfsplus:$tmpdir/hfsplus-tools"
 
 cd $tmpdir/dmg
 
+cp ${tbb_version_type}.DS_Store .DS_Store
+rm *.DS_Store
+
 tar -xf $macos_stapled_dir/"${proj_name}-${tbb_version}-notarized+stapled.tar.zst"
 
 cd ..